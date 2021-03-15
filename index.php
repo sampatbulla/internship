7cf39e9506fe6dd502c6c43ac4c0641482e2ee12
Sampat Kumar <ksampat604@gmail.com>
2021-03-15 11:17:33 +0530

    first change

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<title>LOGIN</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
 <body>
      <form action="login.php" method="post">
--- signup.php
+++ signup.php
@@ -3,6 +3,8 @@
 <head>
 	<title>SIGN UP</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
 <body>
      <form action="signup-check.php" method="post">
